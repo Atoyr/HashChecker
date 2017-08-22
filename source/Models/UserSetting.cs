d69cb50e285043f384beb3d33bef9f35dfccc44a
r.uchiyama <r.uchiyama@sint.co.jp>
2017-08-22 20:04:48 +0900

    komitto

--- source/Models/UserSetting.cs
+++ source/Models/UserSetting.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HashChecker.Models
 {
+    [DataContract]
     public class UserSetting
     {
+        [DataMember(Name = "FirstFolderPath")]
+        public List<string> FirstFolderPath { set; get; }
+        [DataMember(Name = "SecondFolderPath")]
+        public List<string> SecondFolderPath { set; get; }
+
+        public List<string> Filter { set; get; }
     }
 }
